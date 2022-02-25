22ca9824ee260fb8acaf2a9d09efd863e77dc7a6
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-02-25 19:11:35 +0100

    Update pongMenu.c

--- pongMenu.c
+++ pongMenu.c
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int menu1 = 1;
+int menu_chooser = 0;
 float menu_options = 1;
 
 void menu()
 {
+  while(menu1 == 1)
   display_string(1, "PONG");
   display_string(1, "Singleplayer");
   display_string(2, "Multiplayer");
