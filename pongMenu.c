91c6fb4b6eec73e762063bff5e27795c3c0db93f
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-02-26 15:31:53 +0100

    Update pongMenu.c

--- pongMenu.c
+++ pongMenu.c
@@ -4,17 +4,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int menu1 = 1;
+
 int menu_chooser = 0;
-float menu_options = 1;
+int dogmenu_running = 0;
 
 void menu()
 {
-  while(menu1 == 1)
-  display_string(1, "PONG");
-  display_string(1, "Singleplayer");
-  display_string(2, "Multiplayer");
-  display_string(3, "Inverse pong");
+  delay(150);      // without delay it gets updated to fast(since we dont have a timer
   
+  
+  if(menu_chooser == 0)
+  {
+	  display_string(0, "->Main Menu");
+	  display_string(1, "  Singleplayer");
+	  display_string(2, "  Multiplayer");
+  }	  
+  else if(menu_chooser == 1)
+  {
+	  display_string(0, "  Main Menu");
+	  display_string(1, "->Singleplayer");
+	  display_string(2, "  Multiplayer");
+  }	  
+  else if(menu_chooser == 2)
+  {
+	  display_string(0, "  Main Menu");
+	  display_string(1, "  Singleplayer");
+	  display_string(2, "->Multiplayer");
+	  
+	  if( getbtns() & 0x1)
+		dogmenu_running = 1;
+  }	  
+  
+  if ((getbtns() & 0x2) && (menu_chooser < 2))
+	  menu_chooser++;
+  else if ((getbtns() & 0x4) && (menu_chooser > 0))
+	  menu_chooser--;
   display_update();
+}
+
+void dogmenu()
+{
+	display_image(96, icon);
+	if( getbtns() & 0x1)
+		dogmenu_running = 0;
 }
