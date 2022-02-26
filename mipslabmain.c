3e9d33f972326e88f3327243007527ef45aba024
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-02-26 15:57:52 +0100

    Update mipslabmain.c

--- mipslabmain.c
+++ mipslabmain.c
@@ -10,6 +10,7 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "mipslab.h"  /* Declatations for these labs */
+#include "pong.h"
 
 int main(void) {
         /*
