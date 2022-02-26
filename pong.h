6ac8b5b9b4b015dbe8acff2330c140f99434efc7
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-02-26 17:18:37 +0100

    Update pong.h

--- pong.h
+++ pong.h
@@ -1,7 +1,19 @@
-// Declaration of menu functions
+/***************************
+AI Pong header file
+Written by @Isak Wilkens
+**************************/
+// Menu functions
 void menu();
 void dogmenu();
 
 // Menu variables
 extern int menu_chooser;
 extern int dogmenu_running;
+
+// Screen defenitions
+extern uint8_t display_array[32][128];
+extern uint8_t	display_pixels[512];
+
+// Screen functions
+void display_clear();
+void string_clear();
