f3d119555373c2a611aa50f3d7d3a84e1c6b1579
axellokrantz <100090211+axellokrantz@users.noreply.github.com>
2022-02-28 21:48:56 +0100

    Update collision.c

--- collision.c
+++ collision.c
@@ -5,7 +5,53 @@
 #include <stdlib.h>
 #include "pong.h"
 
+// ** RULES FOR COLLISION **
+// ** ANGLE VS SPEED? **
+
 void paddle1_collision()
 {
   
-}
+// ** PADDLE MOVING UPWARDS **
+// If the ball is coming from an upward angle ball angle = * 2.
+// If the ball is coming from a downward angle ball angle = / 2.
+// If the ball is coming from a 0 degree angle = nudge ball upwards.
+  
+  if(paddle1_up == 1)
+    if(ball_speed_y < 0)
+    {
+      ball_speed_y = ball_speed_y * 2; 
+    }
+    else if(ball_speed_y > 0)
+    {
+      ball_speed_y = ball_speed_y / 2;
+    }
+    else
+    {
+      ball_speed_y = -1;
+    }
+  
+// ** PADDLE MOVING DOWNWARDS **
+// If ball is coming from an upward angle ball angle = * 2.
+// If ball is coming from a downward angle ball angle = / 2.
+// If ball is coming from a 0 degree angle = nudge ball downwards.
+  
+  if(paddle1_down == 1)
+    if(ball_speed_y < 0)
+    {
+      ball_speed_y = ball_speed_y / 2;
+    }
+    else if (ball_speed_y > 0)
+    {
+      ball_speed_y = ball_speed_y * 2
+    }
+    else
+    {
+      ball_speed = 1;
+    }
+   }
+  }
+
+
+
+
+
