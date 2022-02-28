2da45132f55a159ba5f1bbc879426e627b01c4bb
axellokrantz <100090211+axellokrantz@users.noreply.github.com>
2022-02-28 16:07:34 +0100

    Update pong.h

--- pong.h
+++ pong.h
@@ -1,18 +1,14 @@
 /***************************
 AI Pong header file
-Written by @Isak Wilkens
+Written by @Isak Wilkens,
+@Axel Lokrantz Månson
 **************************/
+
+// ** Functions **
+
 // Menu functions
 void menu();
 void dogmenu();
-
-// Menu variables
-extern int menu_chooser;
-extern int dogmenu_running;
-
-// Screen defenitions
-extern uint8_t display_array[32][128];
-extern uint8_t	display_pixels[512];
 
 // Screen functions
 void display_clear();
@@ -23,3 +19,39 @@ void castToScreen();
 void singleplayer_motion();
 void multiplayer_motion();
 void computer_motion();
+
+// ** Global variables **
+
+// Menu variables
+extern int menu_chooser;
+extern int dogmenu_running;
+
+// Screen definitions
+extern uint8_t display_array[32][128];
+extern uint8_t	display_pixels[512];
+
+// Motion variables
+
+// Paddle
+extern float paddle_height;
+extern float paddle_width;
+extern float paddle_speed;
+
+extern float paddle1_y;
+extern float paddle1_x;
+extern float paddle1_up;
+extern float paddle1_down;
+
+extern float paddle2_y;
+extern float paddle2_x;
+extern float paddle2_up;
+extern float paddle2_down;
+
+// Ball
+extern float ball_shape;
+extern float ball_speed_y;
+extern float ball_speed_x;
+extern int ball_y;
+extern int ball_x;
+
+extern int count;
