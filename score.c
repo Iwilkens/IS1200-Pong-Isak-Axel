d4fddcf8aa8425433279636fa84ceda3d93c69a8
axellokrantz <100090211+axellokrantz@users.noreply.github.com>
2022-03-01 11:21:37 +0100

    Update score.c

--- score.c
+++ score.c
@@ -1,6 +1,11 @@
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pong.h"
 
-
-
+/*
 int score_player1 = 0;
 int score_player2 = 0;
 
@@ -20,4 +25,25 @@ void score (player)
     display_update();
     delay(2000);
   }
+}
+*/
+  
+void reset()
+{
+  // Resets position & values of paddles + ball.
+  paddle1_x = 0;
+  paddle1_y = 32/2;  
+  paddle2_x = 128 - 4;
+  paddle1_y = 32/2;
+  ball_y = 32/2;
+  ball_x = 128/2;
+  
+  ball_speed_x = 1;
+  ball_speed_y = 0;
+  
+  display_clear();
+  set_displayArray(paddle1_x, paddle1_y, paddle_width, paddle_height);
+  set_displayArray(paddle2_x, paddle2_y, paddle_width, paddle_height);'
+  castToScreen();
+  display_image(0, display_pixels);
 }
