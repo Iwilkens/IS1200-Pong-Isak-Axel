d47ca40ec3f83b31c3c9911571900e05355dcfeb
axellokrantz <100090211+axellokrantz@users.noreply.github.com>
2022-03-01 10:48:57 +0100

    Update score.c

--- score.c
+++ score.c
@@ -3,3 +3,21 @@
 
 int score_player1 = 0;
 int score_player2 = 0;
+
+void score (player)
+{
+  if(player == 1)
+  {
+    score_player1 += 1;
+    display_string("Player one: score!")
+    display_update();
+    delay(2000);
+  }
+  else if (player == 2)
+  {
+    score_player2 += 1;
+    display_string("Player two: score!")
+    display_update();
+    delay(2000);
+  }
+}
