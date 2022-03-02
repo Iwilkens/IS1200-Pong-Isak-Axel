6a5ae39dc4eb82bee60ba8af947cfe191a2c7ad6
Iwilkens <90352345+Iwilkens@users.noreply.github.com>
2022-03-02 18:09:38 +0100

    Update score.c

--- score.c
+++ score.c
@@ -5,57 +5,61 @@
 #include <stdlib.h>
 #include "pong.h"
 
+
 int score_player1 = 0;
 int score_player2 = 0;
 
-void score (player)
+// updates score and displays text for points and win
+void score (int paddle)
 {
-  if(player == 1)
+  if(paddle == 1)
   {
     score_player1 += 1;
     if(score_player1 == 3)
     {
-      display_string("Player one WINS!");
+      display_string(0,"Player one WINS!");
       display_update();
       delay(2000);
-      // back2menu();
+      quit();
     }
     else
     {
-      display_string("Player one score!")
+      display_string(0,"Player one score!");
       display_update();
       delay(2000);
-      // reset();
+      reset();
     }
   }
-  else if (player == 2)
+  else if (paddle == 2)
   {
     score_player2 += 1;
     if (score_player2 == 3)
     {
-      display_string("Player two WINS!");
+      display_string(0, "Player two WINS!");
       display_update();
       delay(2000);
-      // back2menu();
+      quit();
     }
-    display_string("Player two: score!")
+    display_string(0, "Player two score!");
     display_update();
     delay(2000);
-    // reset();
+    reset();
   }
 }
+
   
 void reset()
 {
   // Resets position & values of paddles + ball.
+  // also inverts speed of ball in x direction so a new player hits the ball first.
   paddle1_x = 0;
   paddle1_y = 32/2;  
   paddle2_x = 128 - 4;
-  paddle1_y = 32/2;
+  paddle2_y = 32 /2;
   ball_y = 32/2;
   ball_x = 128/2;
   
-  ball_speed_x = 1;
+  ball_speed_x = (ball_speed_x * -1);
   ball_speed_y = 0;
   
   display_clear();
@@ -64,4 +68,5 @@ void reset()
   set_displayArray(ball_x, ball_y, ball_shape, ball_shape);
   castToScreen();
   display_image(0, display_pixels);
+  delay(1000);
 }
