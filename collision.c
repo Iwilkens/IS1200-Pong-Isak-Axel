46b0089d980a071de3e58d4c956fd41bc57346e4
axellokrantz <100090211+axellokrantz@users.noreply.github.com>
2022-03-03 16:39:52 +0100

    Update collision.c

--- collision.c
+++ collision.c
@@ -5,11 +5,7 @@
 #include <stdlib.h>
 #include "pong.h"
 
-
-
 // ** RULES FOR COLLISION **
-// ** ANGLE VS SPEED? **
-
 
 void paddle1_collision()
 {
@@ -17,7 +13,7 @@ void paddle1_collision()
 // ** PADDLE MOVING UPWARDS **
 // If the ball is coming from an upward angle ball angle = * 2.
 // If the ball is coming from a downward angle ball angle = / 2.
-// If the ball is coming from a 0 degree angle = nudge ball.
+// If the ball is coming from a 0 degree angle = send ball in upward y-motion.
   
   if(paddle1_up == 1)
   {
@@ -40,7 +36,7 @@ void paddle1_collision()
 // ** PADDLE MOVING DOWNWARDS **
 // If ball is coming from an upward angle ball angle = * 2.
 // If ball is coming from a downward angle ball angle = / 2.
-// If ball is coming from a 0 degree angle = nudge ball.
+// If the ball is coming from a 0 degree angle = send ball in downward y-motion.
   
   if(paddle1_down == 1)
   {
@@ -95,24 +91,24 @@ void paddle2_collision()
 // Function to detect collision between ball and paddle1.
 void ball_collision()
 {
-// X coordinate satisfied.
+  // X coordinate satisfied.
   if(ball_x == paddle_width)
   {
-	// Y coordinate satisfied.
+    // Y coordinate satisfied.
     if (((ball_y + ball_shape) > paddle1_y) && (ball_y - ball_shape) < (paddle1_y + paddle_height))
     {
-	  // Both Y & X satisfied = hit. Change direction of ball.
+      // Both Y & X satisfied = hit. Change direction of ball.
       ball_speed_x = (ball_speed_x * -1);
-	  // Run logic for paddle collision.
+      // Run logic for paddle collision.
       paddle1_collision();
     }
   }
+  // No collision. Ball of bounds = score for player 1.	 
   else if(ball_x < -21)
   {
-	  // run score function if paddle misses
+	  // Run score function with value of player who score
 	  score(1);
   }  
-    // 
   if (ball_x == (128 - paddle_width - 4))
   {
     if (((ball_y + ball_shape) > paddle2_y) && (ball_y - ball_shape) < (paddle2_y + paddle_height))
@@ -121,8 +117,10 @@ void ball_collision()
         paddle2_collision();
     }
   }
+  // No collision. Ball of bounds = score for player 1.	 
   else if (ball_x > 138)
   {
+	  // Run score function with value of player who score
 	  score(2);
   }
  }
